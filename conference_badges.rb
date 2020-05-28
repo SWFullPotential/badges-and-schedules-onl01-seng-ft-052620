8dd93ff1b37a6233ef06cc35cda59ee9037b6656
Samantha Wabi <swfullpotential@gmail.com>
2020-05-28 18:43:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,6 +6,12 @@ def batch_badge_creator(array)
    array.each{|attendee| badges << "Hello, my name is #{attendee}."}
 return badges
 end
-def assign_rooms (attendees)
+def assign_rooms(attendees)
  attendees.each_with_index.collect{|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+def printer(attendees)
+ batch_badge_creator(attendees).each do |b| puts b
+ end
+ assign_rooms(attendees).each do |a| puts a
+ end
 end
