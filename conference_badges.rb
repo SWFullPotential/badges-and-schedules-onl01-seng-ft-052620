4fc75e4fbcf824a1318ce08b0ae89d88d232e2de
Samantha Wabi <swfullpotential@gmail.com>
2020-05-28 18:02:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,6 @@ end
 # Write a batch_badge_creator method that takes an array of names as an argument and returns an array of badge messages.
 
 def batch_badge_creator(array)
+  badges = []
   array.each{|attendee| "Hello, my name is #{attendee}."}
 end
